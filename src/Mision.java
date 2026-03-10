183938e7ac4b2aa5d5bd33049c0556530a9fdd35
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-10 09:01:05 +0100

    (feat) Mision: Atributos creados, constructor, getters y toString

--- src/Mision.java
+++ src/Mision.java
@@ -1,5 +1,62 @@
 public class Mision {
 
+    private int id;
+    private String nombre;
+    private String dificultad;
+    private int nivelRecomandado;
+    private int recompensaExperiencia;
+    private boolean completada;
+
+    public Mision(int id, String nombre, String dificultad,  int nivelRecomandado, int recompensaExperiencia, boolean completada) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dificultad = dificultad;
+        this.nivelRecomandado = nivelRecomandado;
+        this.recompensaExperiencia = recompensaExperiencia;
+        this.completada = completada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public int getNivelRecomandado() {
+        return nivelRecomandado;
+    }
+
+    public int getRecompensaExperiencia() {
+        return recompensaExperiencia;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Mision: {");
+
+        sb.append("id=").append(id);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", dificultad=").append(dificultad);
+        sb.append(", nivelRecomandado=").append(nivelRecomandado);
+        sb.append(", recompensaExperiencia=").append(recompensaExperiencia);
+        sb.append(", completada=").append(completada);
+
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+
 }
 
 
